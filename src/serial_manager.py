9cb3c6c04a4a016fec1a99cbd0da1fa4e2473b99
Brian De Winne <dewinnebrian@gmail.com>
2024-06-06 01:08:47 +0200

    fixed issues with volume changes

--- src/serial_manager.py
+++ src/serial_manager.py
@@ -13,21 +13,21 @@ def list_ports():
     ports = sp_list_ports.comports()
     return [port.device for port in ports]
 
-def adjust_volume(application_name, increase=True):
-    sessions = AudioUtilities.GetAllSessions()
-    for session in sessions:
-        if session.Process and session.Process.name() == application_name:
-            volume = session.SimpleAudioVolume
-            current_volume = volume.GetMasterVolume()
-            if increase:
-                new_volume = min(current_volume + 0.1, 1.0)
-            else:
-                new_volume = max(current_volume - 0.1, 0.0)
-            volume.SetMasterVolume(new_volume, None)
-            return new_volume * 100  # Return volume as percentage
-    return None
-
 class VolumeManager:
+    def adjust_volume(self, application_name, increase=True):
+        sessions = AudioUtilities.GetAllSessions()
+        for session in sessions:
+            if session.Process and session.Process.name() == application_name:
+                volume = session.SimpleAudioVolume
+                current_volume = volume.GetMasterVolume()
+                if increase:
+                    new_volume = min(current_volume + 0.1, 1.0)
+                else:
+                    new_volume = max(current_volume - 0.1, 0.0)
+                volume.SetMasterVolume(new_volume, None)
+                return new_volume * 100  # Return volume as percentage
+        return None
+
     def get_available_processes(self):
         sessions = AudioUtilities.GetAllSessions()
         processes = set()
